2993de09ae1b5ceae66a5e3b3dc52a16cfe0a774
MarianaKoval <118778892+MarianaKoval@users.noreply.github.com>
2023-03-14 12:27:59 +0100

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -18,8 +18,7 @@ import java.math.BigDecimal;
         }
     }
     class CalculatorFrame extends JFrame {
-        public CalculatorFrame() {
-            setTitle("Калькулятор java-lessons.ru");
+        public CalculatorFrame() 
             CalculatorPanel panel = new CalculatorPanel();
             add(panel);
             pack();
